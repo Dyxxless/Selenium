63748e4cd61fb8802f7b0d500c9737d8c6d1b15e
Dyxxless <79612696784@yandex.ru>
2022-12-24 00:27:06 +0300

    Some changes

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -3,23 +3,30 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll(){
+    static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
     }
 
     @BeforeEach
-    void setUp(){
-        driver = new ChromeDriver();
+    void setUp() {
+        //driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
-    void tearsDown(){
+    void tearsDown() {
         driver.quit();
-        driver.null;
+        driver = null;
     }
 }
