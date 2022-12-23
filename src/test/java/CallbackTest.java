0f53a5a4544e7a5599f4173a83b51f0b385fe049
Dyxxless <79612696784@yandex.ru>
2022-12-24 01:52:21 +0300

    Add working test

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,16 +1,21 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallbackTest {
     private WebDriver driver;
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dyxxless\\Desktop\\IntelliJ\\Selenium\\chromedriver.exe");
     }
 
     @BeforeEach
@@ -21,12 +26,22 @@ public class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
     void tearsDown() {
         driver.quit();
         driver = null;
+    }
+
+    @Test
+    void test() {
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79991112233");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
